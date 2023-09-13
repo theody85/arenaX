f68559557171db105122f2f4fb23ecd23ba98b97
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-13 20:10:50 +0000

    add frontend structure

--- packages/nextjs/pages/discover.tsx
+++ packages/nextjs/pages/discover.tsx
@@ -1,15 +1,12 @@
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { ContractData } from "~~/components/example-ui/ContractData";
-import { ContractInteraction } from "~~/components/example-ui/ContractInteraction";
+import { ContractData } from "~~/components/discover/ContractData";
+import { ContractInteraction } from "~~/components/discover/ContractInteraction";
 
 const ExampleUI: NextPage = () => {
   return (
     <>
-      <MetaHeader
-        title="Example UI | Scaffold-ETH 2"
-        description="Example UI created with 🏗 Scaffold-ETH 2, showcasing some of its features."
-      >
+      <MetaHeader title="ArenaX | Discover NFTs" description="Buy and sell NFTs in various categories.">
         {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
