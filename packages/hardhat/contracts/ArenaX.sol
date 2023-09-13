850948bed6957f3dca8baef5c284654b44ee5fc1
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-13 18:21:15 +0000

    add completed contract

--- packages/hardhat/contracts/ArenaX.sol
+++ packages/hardhat/contracts/ArenaX.sol
@@ -1,55 +1,72 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-abstract contract AssetTokenization is ERC721 {
+abstract contract AXMarketPlace is ERC1155 {
 	// A struct to represent each tokenized asset
 	struct Asset {
+		uint256 id;
 		string title;
 		string description;
-		uint256 totalShares;
+		string category;
+		uint256 totalSupply;
 		uint256 decimals;
 		uint256 pricePerToken;
 		bool isShared;
 	}
 
-	struct Owner {
-		address ownerAddress;
-		uint256 shareCount;
-	}
+	Asset[] public assets;
 
-	Asset[] assets;
-	uint256 assetCount;
+	mapping(uint256 => address) public creators;
 
-	//Mapping to track shared owner of each asset
-	mapping(uint256 => Owner[]) sharedOwners;
-	Owner[] assetOwners;
+	// Mapping from asset ID to its supply
+	mapping(uint256 => uint256) public assetSupply;
+
+	mapping(uint256 => address[]) owners;
+
+	event AssetMinted(
+		uint256 indexed _assetId,
+		uint256 _quantity,
+		address _owner
+	);
+
+	event AssetTransfered(
+		uint256 _assetId,
+		uint256 _quantity,
+		address _owner,
+		address _newowner
+	);
 
 	// Function to mint new asset tokens
 	function mintAsset(
 		string memory _title,
 		string memory _description,
-		uint256 _totalShares,
+		string memory _category,
+		uint256 _totalSupply,
 		uint256 _decimals,
 		uint256 _pricePerToken,
-		bool _isShared
+		bool _isShared,
+		bool _isApproved
 	) external {
+		require(_isApproved, "You need to grant  operation rights");
+
 		if (!_isShared) {
 			require(
-				_totalShares == 1,
+				_totalSupply == 1,
 				"Total supply of indivisible assets must be 1"
 			);
 		}
 
-		uint256 tokenId = assetCount++;
+		uint256 assetId = assets.length + 1;
 
 		assets.push(
 			Asset({
+				id: assetId,
 				title: _title,
 				description: _description,
-				totalShares: _totalShares,
+				category: _category,
+				totalSupply: _totalSupply,
 				decimals: _decimals,
 				pricePerToken: _pricePerToken,
 				isShared: _isShared
@@ -57,82 +74,108 @@ abstract contract AssetTokenization is ERC721 {
 		);
 
 		_isShared
-			? _mintSharedAsset(msg.sender, tokenId, _totalShares)
-			: _safeMint(msg.sender, tokenId);
+			? _mintSharedAsset(msg.sender, assetId, _totalSupply)
+			: _mintNonSharedAsset(msg.sender, assetId);
 
-		//check for event Transfer(address(0), to, tokenId);
+		assetSupply[assetId] = _totalSupply;
+
+		setApprovalForAll(address(this), _isApproved);
+	}
+
+	modifier validID(uint _id) {
+		require(_id > 0 && _id <= assets.length, "Invalid asset ID");
+		_;
+	}
+
+	modifier isShared(uint256 _quantity) {
+		require(_quantity > 1, "Quantity must be greater than 1");
+		_;
 	}
 
 	function _mintSharedAsset(
-		address to,
-		uint256 tokenId,
-		uint _shareCount
-	) internal virtual {
-		require(to != address(0), "ERC721: mint to the zero address");
-		require(!_exists(tokenId), "ERC721: token already minted");
-
-		_beforeTokenTransfer(address(0), to, tokenId, 1);
-
-		// Check that tokenId was not minted by `_beforeTokenTransfer` hook
-		require(!_exists(tokenId), "ERC721: token already minted");
-
-		assetOwners.push(
-			Owner({ ownerAddress: msg.sender, shareCount: _shareCount })
-		);
-
-		sharedOwners[tokenId] = assetOwners;
-
-		emit Transfer(address(0), to, tokenId);
-		_afterTokenTransfer(address(0), to, tokenId, 1);
-	}
-
-	// Function to buy tokens representing fractional ownership
-	function buyAsset(uint256 _tokenId, uint256 _amount) external payable {
-		require(_amount > 0, "Amount must be greater than 0");
-		require(_tokenId <= assetCount, "Invalid token ID");
-		require(assets[_tokenId - 1].pricePerToken * _amount <= msg.value);
-
-		assets[_tokenId - 1].isShared
-			? _buySharedAsset(_tokenId, _amount)
-			: _buyAsset(_tokenId);
-	}
-
-	function _buySharedAsset(uint256 _tokenId, uint256 _amount) internal {
-		require(
-			sharedOwners[_tokenId][0].shareCount >= _amount,
-			"Not enough shares to be sold"
-		);
-
-		// Transfer tokens to the buyer
-		_transferTokens(msg.sender, _tokenId, _amount);
-
-		// Transfer funds to the owner of the asset
-		_transferFunds(
-			payable(sharedOwners[_tokenId][0].ownerAddress),
-			msg.value
-		);
-	}
-
-	function _buyAsset(uint _tokenId) internal {
-		_transfer(ownerOf(_tokenId), msg.sender, _tokenId);
-
-		// Transfer funds to the owner of the asset
-		_transferFunds(payable(ownerOf(_tokenId)), msg.value);
-	}
-
-	function _transferTokens(
 		address _to,
-		uint256 _tokenId,
-		uint256 _amount
-	) internal {
-		require(_amount > 0, "Amount must be greater than 0");
-		require(_to != address(0), "Invalid recipient address");
+		uint256 _assetId,
+		uint256 _quantity
+	) internal validID(_assetId) isShared(_quantity) {
+		uint256[] memory ids = new uint256[](1);
+		uint256[] memory amounts = new uint256[](1);
+		ids[0] = _assetId;
+		amounts[0] = _quantity;
 
-		sharedOwners[_tokenId][0].shareCount -= _amount;
+		_mintBatch(_to, ids, amounts, "");
 
-		sharedOwners[_tokenId].push(
-			Owner({ ownerAddress: _to, shareCount: _amount })
+		creators[_assetId] = _to;
+		emit AssetMinted(_assetId, _quantity, _to);
+	}
+
+	function _mintNonSharedAsset(
+		address _to,
+		uint256 _assetId
+	) internal validID(_assetId) {
+		_mint(_to, _assetId, 1, "");
+
+		creators[_assetId] = _to;
+		emit AssetMinted(_assetId, 1, _to);
+	}
+
+	function buyAsset(
+		uint256 _assetId,
+		uint256 _quantity
+	) external payable validID(_assetId) {
+		require(_quantity > 0, "Number of token must be greater than 0");
+		require(
+			assetSupply[_assetId] >= _quantity,
+			"Not enough tokens left to sell"
 		);
+
+		require(assets[_assetId - 1].pricePerToken * _quantity <= msg.value);
+
+		assets[_assetId - 1].isShared
+			? _buySharedAsset(_assetId, _quantity)
+			: _buyNonSharedAsset(_assetId);
+	}
+
+	function _buySharedAsset(
+		uint256 _assetId,
+		uint256 _quantity
+	) internal validID(_assetId) isShared(_quantity) {
+		// Transfer the ERC-1155 tokens to the buyer
+		uint256[] memory ids = new uint256[](1);
+		uint256[] memory amounts = new uint256[](1);
+		ids[0] = _assetId;
+		amounts[0] = _quantity;
+
+		_safeBatchTransferFrom(
+			creators[_assetId],
+			msg.sender,
+			ids,
+			amounts,
+			""
+		);
+		_transferFunds(payable(creators[_assetId]), msg.value);
+
+		// Update the asset supply
+		assetSupply[_assetId] -= _quantity;
+		owners[_assetId].push(msg.sender);
+
+		emit AssetTransfered(
+			_assetId,
+			_quantity,
+			creators[_assetId],
+			msg.sender
+		);
+	}
+
+	function _buyNonSharedAsset(uint256 _assetId) internal validID(_assetId) {
+		// Transfer the tokens
+		_safeTransferFrom(creators[_assetId], msg.sender, _assetId, 1, "");
+
+		_transferFunds(payable(creators[_assetId]), msg.value);
+
+		assetSupply[_assetId] -= 1;
+		owners[_assetId].push(msg.sender);
+
+		emit AssetTransfered(_assetId, 1, creators[_assetId], msg.sender);
 	}
 
 	function _transferFunds(address payable _to, uint _amount) internal {
@@ -140,33 +183,29 @@ abstract contract AssetTokenization is ERC721 {
 		require(sent, "Failed to send Ether");
 	}
 
-	//Function to get all assets
-	function getAssets() public view returns (Asset[] memory) {
+	function getAssetsLeft(
+		uint256 _id
+	) external view validID(_id) returns (uint256) {
+		return assetSupply[_id];
+	}
+
+	function getOwners(
+		uint256 _id
+	) external view validID(_id) returns (address[] memory) {
+		return owners[_id];
+	}
+
+	function getAllAssets() external view returns (Asset[] memory) {
 		return assets;
-	}
-
-	modifier exists(uint256 _tokenId) {
-		require(_tokenId <= assetCount, "Invalid token ID");
-		require(assets[_tokenId - 1].totalShares != 0, "Invalid token ID");
-		_;
-	}
-
-	//Function to get shared owners of a particular asset
-	function getSharedOwners(
-		uint256 _tokenId
-	) public view exists(_tokenId) returns (Owner[] memory) {
-		require(sharedOwners[_tokenId].length > 0, "Invalid asset ID");
-
-		return sharedOwners[_tokenId];
 	}
 
 	// Function to retrieve asset details
 	function getAsset(
-		uint256 _tokenId
+		uint256 _id
 	)
 		external
 		view
-		exists(_tokenId)
+		validID(_id)
 		returns (
 			string memory title,
 			string memory description,
@@ -175,46 +214,43 @@ abstract contract AssetTokenization is ERC721 {
 			uint256 pricePerToken
 		)
 	{
-		Asset memory asset = assets[_tokenId - 1];
+		Asset memory asset = assets[_id - 1];
 		return (
 			asset.title,
 			asset.description,
-			asset.totalShares,
+			asset.totalSupply,
 			asset.decimals,
 			asset.pricePerToken
 		);
 	}
 
-	modifier onlyOwner(uint256 _tokenId) {
-		Asset memory asset = assets[_tokenId - 1];
+	modifier onlyCreator(uint256 _assetId) {
+		Asset memory asset = assets[_assetId - 1];
 
-		if (asset.isShared) {
-			require(
-				sharedOwners[_tokenId].length == 1,
-				"Requires more than one owner to grant access"
-			);
-			require(
-				msg.sender == sharedOwners[_tokenId][0].ownerAddress,
-				"Unauthorised access"
-			);
-		} else {
-			require(msg.sender == ownerOf(_tokenId), "Unauthorised access");
-		}
+		require(owners[_assetId].length == 0, "Unauthorised access");
+		require(msg.sender == creators[_assetId], "Unauthorised access");
 
 		_;
 	}
 
 	// Function to update asset details
 	function updateAssetDetails(
-		uint256 _tokenId,
+		uint256 _assetId,
 		string memory _title,
 		string memory _description,
 		uint256 _pricePerToken
-	) external exists(_tokenId) onlyOwner(_tokenId) {
-		require(_tokenId <= assetCount, "Invalid token ID");
+	) external validID(_assetId) onlyCreator(_assetId) {
+		assets[_assetId - 1].title = _title;
+		assets[_assetId - 1].description = _description;
+		assets[_assetId - 1].pricePerToken = _pricePerToken;
+	}
 
-		assets[_tokenId - 1].title = _title;
-		assets[_tokenId - 1].description = _description;
-		assets[_tokenId - 1].pricePerToken = _pricePerToken;
+	function burnAsset(
+		uint256 _assetId
+	) external validID(_assetId) onlyCreator(_assetId) {
+		// Burn the tokens
+		_burn(creators[_assetId], _assetId, assetSupply[_assetId]);
+
+		creators[_assetId] = address(0);
 	}
 }
