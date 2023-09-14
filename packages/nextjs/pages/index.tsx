c580977974871115825ec48b7ca13dbd36b9d7ed
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-14 00:11:26 +0000

    :sparkles: Rework on feature

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,69 +1,49 @@
-import Link from "next/link";
+import Image from "next/image";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
+import hero1 from "~~/public/assets/hero-1.png";
+import hero2 from "~~/public/assets/hero-2.png";
+import hero3 from "~~/public/assets/hero-3.png";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+      <div className="flex items-center flex-col flex-grow pt-10 h-screen">
+        <div className="flex w-full justify-between relative">
+          <div className="flex flex-col gap-y-4">
+            <div className="font-michroma text-white text-7xl lg:w-2/3nline-flex lg:leading-normal">
+              NFT MARKET PLACE 🔥
+            </div>
+
+            <div className="flex gap-x-2 w-full">
+              <div className="text-white font-michroma text-lg px-3 py-3 rounded-lg bg-transparent border border-[#23CEFD] w-36 text-center">
+                #CREATE
+              </div>
+              <div className="text-white font-michroma text-lg px-3 py-3 rounded-lg bg-transparent border border-[#23CEFD] w-36 text-center">
+                #BUY
+              </div>
+              <div className="text-white font-michroma text-lg px-3 py-3 rounded-lg bg-transparent border border-[#23CEFD] w-36 text-center">
+                #SELL
+              </div>
+            </div>
+
+            <h3 className="text-white text-2xl font-semibold tracking-wide">AND DISCOVER RARE DIGITAL ITEMS</h3>
+          </div>
+          <div className="w-full">
+            <Image src={hero1} alt="hero" className="absolute top-0 right-0 z-50" width={300} height={300} />
+            <Image src={hero2} className="absolute -top-6 right-24 z-40" alt="hero" width={300} height={300} />
+            <Image src={hero3} className="absolute -top-11 right-52 z-30" alt="hero" width={300} height={300} />
+          </div>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+        <div className="w-full flex items-center justify-center gap-x-8 mt-12 lg:mt-24">
+          <button className="gradient-bg font-michroma uppercase text-xl px-12 py-4 text-white border border-[#23CEFD] rounded-xl">
+            discover
+          </button>
+          <button className="font-michroma uppercase text-xl px-12 py-4 bg-transparent text-white border border-[#23CEFD] rounded-xl">
+            create
+          </button>
         </div>
       </div>
     </>
